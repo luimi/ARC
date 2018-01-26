e580afe78f130ff86348dd91add3ddab5ffdf48a
Luis Meza <luimi@live.com>
2018-01-26 13:56:50 -0500

    correxion en casos de ver lista y no estar logeado o no ser el usuario que lo creo, agregado filtro y el loading en todas las vistas

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,10 @@
 angular.module('app.controllers', [])
   
-.controller('loginCtrl', ['$scope', '$stateParams','$state','$ionicPopup',
+.controller('loginCtrl', ['$scope', '$stateParams','$state','$ionicPopup','$ionicLoading',
 // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $state,$ionicPopup) {
+function ($scope, $stateParams, $state,$ionicPopup,$ionicLoading) {
 	$scope.usuario={};
 	continuar=function(){
 		$state.go('tuLista');
@@ -15,14 +15,17 @@ function ($scope, $stateParams, $state,$ionicPopup) {
 		}
 	});
 	$scope.registrar=function(){
+		$ionicLoading.show();
 		var user = new Parse.User();
 		user.set("username", $scope.usuario.nombre);
 		user.set("password", $scope.usuario.clave);
 		user.signUp(null, {
 		  success: function(user) {
+		  	$ionicLoading.hide();
 		    continuar();
 		  },
 		  error: function(user, error) {
+		  	$ionicLoading.hide();
 		    $ionicPopup.alert({
 		  		title: 'Login',
          		template: 'Usuario ya existe'
@@ -31,11 +34,14 @@ function ($scope, $stateParams, $state,$ionicPopup) {
 		});
 	}
 	$scope.entrar=function(){
+		$ionicLoading.show();
 		Parse.User.logIn($scope.usuario.nombre, $scope.usuario.clave, {
 		  success: function(user) {
+		  	$ionicLoading.hide();
 		    continuar();
 		  },
 		  error: function(user, error) {
+		  	$ionicLoading.hide();
 		    $ionicPopup.alert({
 		  		title: 'Login',
          		template: 'Usuario y/o contraseña invalidos'
@@ -45,29 +51,35 @@ function ($scope, $stateParams, $state,$ionicPopup) {
 	}
 }])
    
-.controller('tuListaCtrl', ['$scope', '$stateParams', '$state','$ionicPopup', '$timeout','Objeto','$window',
+.controller('tuListaCtrl', ['$scope', '$stateParams', '$state','$ionicPopup', '$timeout','Objeto','$window','$ionicLoading',
 // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams,$state ,$ionicPopup, $timeout,Objeto,$window) {
-	$scope.$on('$ionicView.beforeEnter',function(){
-		listar();
-	});
+function ($scope, $stateParams,$state ,$ionicPopup, $timeout,Objeto,$window,$ionicLoading) {
 	var current=Parse.User.current();
 	var id=$state.params.id;
+	$scope.esUsuario=current!=undefined;
+
+	$scope.$on('$ionicView.beforeEnter',function(){
+		listar();
+
+	});
+	
 	var listar=function(){
+		$ionicLoading.show();
 		new Parse.Query("Lista")
 		.equalTo("usuario",current?current:(id?Objeto.id("_User",id):""))
 		.find({
 			success:function(results){
+				$ionicLoading.hide();
 				$scope.listas=results;
 				$scope.$apply();
+
 			}
 		});
 	}
 	$scope.nuevo=function(){
-		if(Parse.User.current())
-			$state.go('nuevaLista');
+		$state.go('nuevaLista');
 	}
 	$scope.borrar=function(index){
 		$ionicPopup.confirm({
@@ -75,8 +87,10 @@ function ($scope, $stateParams,$state ,$ionicPopup, $timeout,Objeto,$window) {
 			template: 'Quieres borrar esta lista?'
 		}).then(function(res){
 			if(res){
+				$ionicLoading.show();
 				$scope.listas[index].destroy({
 					success:function(obj){
+						$ionicLoading.hide();
 						listar();
 					}
 				});
@@ -84,23 +98,24 @@ function ($scope, $stateParams,$state ,$ionicPopup, $timeout,Objeto,$window) {
 		});
 	}
 	$scope.compartir=function(){
-		console.log($window);
 		$window.open("http://lui2mi.rf.gd/ARC/#/lista/"+Parse.User.current().id, "_blank");
 	}
 
 }])
    
 .controller('nuevaListaCtrl', ['$scope', '$stateParams','Objeto','$state',
-	'$ionicHistory','ACL',
+	'$ionicHistory','ACL','$ionicLoading',
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams,Objeto,$state,$ionicHistory,ACL) {
+function ($scope, $stateParams,Objeto,$state,$ionicHistory,ACL,$ionicLoading) {
 	$scope.nuevo={};
 
 	$scope.guardar=function(){
 		if($scope.nuevo.nombre && $scope.nuevo.descripcion){
+			$ionicLoading.show();
 			$scope.nuevo.usuario=Parse.User.current();
 			Objeto.generico("Lista").setACL(ACL()).save($scope.nuevo,{success:function(obj){
+				$ionicLoading.hide();
 				$ionicHistory.goBack();
 			}});
 		}else{
@@ -111,48 +126,57 @@ function ($scope, $stateParams,Objeto,$state,$ionicHistory,ACL) {
 }])
    
 .controller('itemsCtrl', ['$scope', '$stateParams', '$state','cargarLista',
-	'$ionicHistory',
+	'$ionicHistory','$ionicLoading',
 // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams,$state,cargarLista,$ionicHistory) {
+function ($scope, $stateParams,$state,cargarLista,$ionicHistory,$ionicLoading) {
+	$scope.esUsuario=false;
 	$scope.$on('$ionicView.beforeEnter',function(){
 		listarArticulos();
 	});
 	var listarArticulos=function(){
+		$ionicLoading.show();
 		new Parse.Query("Articulo").equalTo("lista",$scope.lista).find({
 			success:function(articulos){
+				$ionicLoading.hide();
 				$scope.articulos=articulos;
 				$scope.$apply();
 			}
 		});
 	}
 	if($state.params.lista){
+		$ionicLoading.show();
 		cargarLista($state.params.lista,$scope,function(){
+			$scope.esUsuario=Parse.User.current() && 
+				$scope.lista.get('usuario').id==Parse.User.current().id;
+			$ionicLoading.hide();
 			listarArticulos();
 		});
 	}else{
 		$ionicHistory.goBack();
 	}
 	$scope.nuevo=function(){
-		if(Parse.User.current())
-			$state.go('item',{lista:$scope.lista.id});
+		$state.go('item',{lista:$scope.lista.id});
 	}
 
 
 }])
    
 .controller('itemCtrl', ['$scope', '$stateParams', 'cargarLista','$state','Objeto',
-	'$ionicPopup','$ionicHistory','$cordovaCamera','ACL',
+	'$ionicPopup','$ionicHistory','$cordovaCamera','ACL','$ionicLoading',
 // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams,cargarLista,$state,Objeto,$ionicPopup,$ionicHistory,
-	$cordovaCamera,ACL) {
+	$cordovaCamera,ACL,$ionicLoading) {
+	$scope.esUsuario=false;
 	$scope.datos={};
 	var cargarArticulo=function(){
+		$ionicLoading.show();
 		new Parse.Query("Articulo").get($state.params.id,{
 			success:function(articulo){
+				$ionicLoading.hide();
 				$scope.articulo=articulo;
 				$scope.datos={
 					marca:articulo.get('marca'),
@@ -166,6 +190,8 @@ function ($scope, $stateParams,cargarLista,$state,Objeto,$ionicPopup,$ionicHisto
 	}
 	if($state.params.lista){
 		cargarLista($state.params.lista,$scope,function(){
+			$scope.esUsuario=Parse.User.current() && 
+				$scope.lista.get('usuario').id==Parse.User.current().id;
 			if($state.params.id)
 				cargarArticulo();
 		});
@@ -180,9 +206,11 @@ function ($scope, $stateParams,cargarLista,$state,Objeto,$ionicPopup,$ionicHisto
 			$scope.articulo.set("lugar",$scope.datos.lugar);
 			$scope.articulo.save();
 		}else{
+			$ionicLoading.show();
 			$scope.datos.lista=$scope.lista;
 			Objeto.generico("Articulo").setACL(ACL()).save($scope.datos,{
 				success:function(obj){
+					$ionicLoading.hide();
 					obj.fetch();
 					$scope.articulo=obj;
 					$scope.$apply();
@@ -196,8 +224,10 @@ function ($scope, $stateParams,cargarLista,$state,Objeto,$ionicPopup,$ionicHisto
 			template: 'Quieres borrar este articulo?'
 		}).then(function(res){
 			if(res){
+				$ionicLoading.show();
 				$scope.articulo.destroy({
 					success:function(){
+						$ionicLoading.hide();
 						$ionicHistory.goBack();
 					}
 				});
@@ -220,11 +250,13 @@ function ($scope, $stateParams,cargarLista,$state,Objeto,$ionicPopup,$ionicHisto
 	    $cordovaCamera.getPicture(options).then(function(imageData) {
 	      var file = new Parse.File($scope.articulo.id+".jpg", 
 	      	{ base64: "data:image/jpeg;base64," + imageData });
+	      $ionicLoading.show();
 	      $scope.articulo.set("foto",file);
 	      $scope.articulo.save(null,{
 	      	success:function(){
 	      		$scope.articulo.fetch({
 	      			success:function(){
+	      				$ionicLoading.hide();
 	      				$scope.$apply();
 	      			}
 	      		})
--- www/js/directives.js
+++ www/js/directives.js
@@ -1,5 +1,18 @@
 angular.module('app.directives', [])
-
+.filter('listafilter', function() {
+  return function(items, search) {
+    if (!search) {
+      return items;
+    }
+    var filtrados=[];
+    for (var i = 0; i < items.length; i++) {
+    	if(items[i].get('nombre').toLowerCase().search(search.toLowerCase())>=0){
+    		filtrados.push(items[i]);
+    	}
+    }
+    return filtrados;
+  };
+})
 .directive('blankDirective', [function(){
 
 }]);
